898766a843fc289a11d96d434cc42a35bb58c849
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-26 15:17:07 +0000

    problem

---
+++ main.sql
@@ -0,0 +1,4 @@
+/*
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
+*/
+
