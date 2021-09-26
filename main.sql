0e7dee1bdb8b3ef03e5623c364b36a8edab46241
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-26 15:20:17 +0000

    return one line - solution

--- main.sql
+++ main.sql
@@ -2,4 +2,4 @@
 Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
 */
 
-select (salary*months) as total_sal, count(*) from employee group by total_sal order by total_sal desc
+select (salary*months) as total_sal, count(*) from employee group by total_sal order by total_sal desc limit 1
