5a2863415e167134c10c136643d662c5bf14e4b2
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-26 15:17:56 +0000

    test calculation

--- main.sql
+++ main.sql
@@ -2,3 +2,4 @@
 Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
 */
 
+select (salary*months) as total_sal from employee
